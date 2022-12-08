6203fe9b06db9205f9ad9bfe96562681935a720d
Ghaarp <xdmail@bk.ru>
2022-12-08 15:34:40 +0300

    small fix

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -4,5 +4,4 @@ export const BESTRESULTS_ROUTE = '/top';
 export const LOGIN_ROUTE = '/login';
 export const REG_ROUTE = '/registration';
 export const TOKEN_STORAGE_KEY = "CardsGame_token";
-//export const REACT_APP_API_URL='http://localhost:7000/'
 export const REACT_APP_API_URL='https://the-sum-game-server.fly.dev/'
